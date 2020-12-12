c3faa98675b186d0a7c8cb39c1530f867a045896
patrickz24 <patrick.zamblebi@yahoo.fr>
2020-12-12 21:54:57 +0100

    page home fini, next step

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="home">
             <div className="home-img">
-                <img src= {`${process.env.PUBLIC_URL}/patrick.jpg`}  alt="patrick"/>
+                <img src= {`${process.env.PUBLIC_URL}/patrick.jpg`}   alt="patrick"/>
             </div>
             <div className="home-description">
                 <h1>DEVELOPPEUR WEB</h1>
